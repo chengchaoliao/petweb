3c96cc051f4366a1e1baf610593d5f9bbe38045e
chengchaoliao <128762585+chengchaoliao@users.noreply.github.com>
2023-03-24 20:47:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,32 +3,25 @@ var item2Qty = 0;
 var item3Qty = 0;
 var item4Qty = 0;
 var item5Qty = 0;
+var totalItems = 0;
+var totalCost = 0;
 
 function incrementItem(itemId) {
   switch (itemId) {
     case 1:
       item1Qty++;
+      totalItems++;
+      totalCost += 1;
       document.getElementById("item1-qty").innerHTML = item1Qty;
       break;
     case 2:
       item2Qty++;
+      totalItems++;
+      totalCost += 2;
       document.getElementById("item2-qty").innerHTML = item2Qty;
       break;
     case 3:
       item3Qty++;
-      document.getElementById("item3-qty").innerHTML = item3Qty;
-      break;
-    case 4:
-      item4Qty++;
-      document.getElementById("item4-qty").innerHTML = item4Qty;
-      break;
-    case 5:
-      item5Qty++;
-      document.getElementById("item5-qty").innerHTML = item5Qty;
-      break;
-  }
-  
-  updateTotal();
-}
-
-function decrementItem
+      totalItems++;
+      totalCost += 3;
+      document.getElementById("item3
