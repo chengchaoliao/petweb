58fb427351aae4e3ee2045b524ad02577eddbaaf
chengchaoliao <128762585+chengchaoliao@users.noreply.github.com>
2023-03-25 13:54:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ function calculate() {
   const item3 = parseInt(document.getElementById('item3').value);
   const item4 = parseInt(document.getElementById('item4').value);
   const item5 = parseInt(document.getElementById('item5').value);
-  const item6 = parseInt(document.getElementById('item6').value;
-  const item7 = parseInt(document.getElementById('item7').value;
-  const item8 = parseInt(document.getElementById('item8').value;
-  const item9 = parseInt(document.getElementById('item9').value;
-  const item10 = parseInt(document.getElementById('item10').value;
-  const item11 = parseInt(document.getElementById('item11').value;
-  const item12 = parseInt(document.getElementById('item12').value;
-  const item13 = parseInt(document.getElementById('item13').value;
+  const item6 = parseInt(document.getElementById('item6').value);
+  const item7 = parseInt(document.getElementById('item7').value);
+  const item8 = parseInt(document.getElementById('item8').value);
+  const item9 = parseInt(document.getElementById('item9').value);
+  const item10 = parseInt(document.getElementById('item10').value);
+  const item11 = parseInt(document.getElementById('item11').value);
+  const item12 = parseInt(document.getElementById('item12').value);
+  const item13 = parseInt(document.getElementById('item13').value);
 
 
   const totalItems = item1+item2+item3+item4+item5+item6+item7+item8+item9+item10+item11+item12+item13;
